11a203e0bddb8c16b21f14763c0f6770c839799f
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-07-01 17:28:13 -0400

    try adding reach router and BorignShell to jamstack-scripts

--- packages/jamstack-scripts/scripts/init.js
+++ packages/jamstack-scripts/scripts/init.js
@@ -149,7 +149,7 @@ module.exports = function(
     command = 'npm';
     args = ['install', '--save', verbose && '--verbose'].filter(e => e);
   }
-  args.push('react', 'react-dom');
+  args.push('react', 'react-dom', '@reach/router');
 
   // Install additional template dependencies, if present
   const templateDependenciesPath = path.join(
